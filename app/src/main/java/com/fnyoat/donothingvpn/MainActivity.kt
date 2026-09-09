7d9b02c70d0cf74c9ed1b9d52d4e55a0960d87f1
fnyoat <fnyoat@users.noreply.github.com>
2026-09-09 12:23:51 +0000

    Fix compile: use VpnManager (API 30+) for always-on VPN check

--- app/src/main/java/com/fnyoat/donothingvpn/MainActivity.kt
+++ app/src/main/java/com/fnyoat/donothingvpn/MainActivity.kt
@@ -4,11 +4,10 @@ import android.Manifest
 import android.app.Activity
 import android.content.Intent
 import android.content.pm.PackageManager
-import android.net.ConnectivityManager
+import android.net.VpnManager
 import android.net.VpnService
 import android.os.Build
 import android.os.Bundle
-import android.os.Process
 import android.os.UserManager
 import android.util.Log
 import android.widget.Button
@@ -104,14 +103,16 @@ class MainActivity : Activity() {
     }
 
     private fun vpnBlockReason(): CharSequence? {
-        try {
-            val cm = getSystemService(ConnectivityManager::class.java)
-            val alwaysOn = cm.getAlwaysOnVpnPackageForUser(Process.myUserHandle())
-            if (alwaysOn != null && alwaysOn != packageName) {
-                return getString(R.string.reason_always_on_vpn, alwaysOn)
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+            try {
+                val vpnManager = getSystemService(VpnManager::class.java)
+                val alwaysOn = vpnManager.getAlwaysOnVpnPackageForUser()
+                if (alwaysOn != null && alwaysOn != packageName) {
+                    return getString(R.string.reason_always_on_vpn, alwaysOn)
+                }
+            } catch (e: Exception) {
+                Log.e(TAG, "always-on vpn check failed", e)
             }
-        } catch (e: Exception) {
-            Log.e(TAG, "always-on vpn check failed", e)
         }
         try {
             val um = getSystemService(UserManager::class.java)
